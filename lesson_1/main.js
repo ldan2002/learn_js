06fa6048b4dbba383505582a2ee9016a4db38fce
ldan2002 <ledaian22@gmail.com>
2022-12-14 01:07:06 +0700

    first commit

---
+++ lesson_1/main.js
@@ -0,0 +1,34 @@
+// Khai báo biến
+/**
+ * var = 'abc'
+ * let = 24
+ * const = 'def'
+ */
+
+// Một số hàm Built-in
+/**
+ * 1.alert => Hiện lên thông báo
+ * 2.console => In ra thông tin bên tab console của trình duyệt
+ * 3.confirm => Hiện lên thông báo và có lựa chọn hủy bỏ hoặc tiếp tục
+ * 4.prompt => Giống alert và confirm nhưng có thêm ô input để nhập giá trị
+ * 5.setTimeout => setTimeout(function() {Sau 1 khoảng thời gian sẽ chạy đoạn code ở đây}, khoảng thời gian)
+ * 6.setInterval => setImterval(function(){Chạy liên tục khoảng thời gian}, khoảng thời gian)
+ */
+
+// Toán tử
+/**
+ * 1. Toán tử số học - Arithmetic => (+, -, *, /, **, /, %, ++, --)
+  {
+    + : Cộng
+    - : Trừ
+    * : Nhân
+    / : Chia
+    **: Lũy thừa
+    % : Chia lấy số dư
+    ++: Tăng giá trị lên 1(số)
+    --: Giảm giá trị 1(số)
+  }
+ * 2. Toán tử gán - Assignment => (=)
+ * 3. Toán tử so sánh - Comparison (>, <, ===,)
+ * 4. Toán tử logic - Logical () (&&, ||, !)
+ */
